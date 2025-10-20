a02931022d9e24383094a9b1ba352077836526cc
Melanyrc19 <melanyrc.19@gmail.com>
2025-10-20 12:02:37 -0300

    ejercicios 3 clase5

--- src/components/clase5./Lista.jsx
+++ src/components/clase5./Lista.jsx
@@ -2,28 +2,26 @@ import React from 'react'
 import { useState } from 'react'
 
 
+function Lista() {
+    const [items, setItems] = useState([])
+    const [nuevoItem, setNuevoItem] = useState('')
 
-const tarea = () => {
+    const agregarItem = () => { 
+        setItems([...items, nuevoItem])
+        setNuevoItem('')
+    }
+
     return (
         <>
+            <input value={nuevoItem} onChange={(e) => setNuevoItem(e.target.value)} type="text" />
+            <button onClick={agregarItem}>Agregar Item</button>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
         </>
     )
-}
- const Lista = () => {
-    const [tareas, setTareas] = useState([])
-
-
-  return (
-    <div>
-        <input onChange={(e) => setTareas(e.target.value)}  type="text" />
-        <button></button>
-        <ul>
-            {tareas.map((tarea, index) => (
-                <li key={index}>{tarea}</li>
-            ))}
-        </ul>
-    </div>
-  )
 }
 
 export default Lista
